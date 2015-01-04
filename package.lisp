62d781adb7d0ecc776b8032f3dd9ea7f09e82804
mrkkrp <markkarpov@opmbx.org>
2015-01-05 00:30:47 +0600

    minor edit

--- package.lisp
+++ package.lisp
@@ -17,11 +17,11 @@
 ;;; You should have received a copy of the GNU General Public License along
 ;;; with this program. If not, see <http://www.gnu.org/licenses/>.
 
-(cl:defpackage :cl-readline
-  (:nicknames  :rl)
-  (:use        #:common-lisp
-               #:alexandria
-               #:cffi)
+(defpackage   :cl-readline
+  (:nicknames :rl)
+  (:use       #:common-lisp
+              #:alexandria
+              #:cffi)
   (:export
    ;; Readline Variables
    #:*line-buffer*
